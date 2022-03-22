4c69a39e9b066eb7c1eb2190ef82537081414fa9
jiyunnoh <ianjy1127@gmail.com>
2022-03-21 19:09:04 -0500

    Adding a ErrorModal comp

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 // Enter username and age. Add button to submit.
@@ -12,7 +13,7 @@ const AddUser = (props: { onAddUser: (arg0: string, arg1: string) => void }) => 
         // when click the submit button, it prevents the request from being sent by default, and the page will not reload.
         // only request when we want. But here, we don't request.
         event.preventDefault();
-        
+
         // trim() removes excess white space.
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
             return;
@@ -34,19 +35,22 @@ const AddUser = (props: { onAddUser: (arg0: string, arg1: string) => void }) => 
     }
 
     return (
-        // have to have 'className' prop in Card.
-        <Card className={classes.input}>
-        {/* If add parentheses, it will execuete when line 11 is parsed. */}
-        <form onSubmit={addUserHandler}>
-            {/* 'for' is already assigned in JavaScript. Instead, use 'htmlFor' */}
-            {/* 'for' is used in labels. It refers to the id of the element this label is associated with */}
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
-            <Button type="submit">Add User</Button>
-        </form>
-        </Card>
+        <div>
+            <ErrorModal title="An error occured!" message="Something went wrong!" />
+            {/* have to have 'className' prop in Card. */}
+            <Card className={classes.input}>
+                {/* If add parentheses, it will execuete when line 11 is parsed. */}
+                <form onSubmit={addUserHandler}>
+                    {/* 'for' is already assigned in JavaScript. Instead, use 'htmlFor' */}
+                    {/* 'for' is used in labels. It refers to the id of the element this label is associated with */}
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     )
 }
 
